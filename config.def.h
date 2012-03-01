e0d5a7dba2365d84f70c55e46732d71e71335090
Ivan Kanakarakis <ivan.kanak@gmail.com>
2012-03-01 19:42:56 +0200

    rotate non-empty desktops with shortcuts

--- config.def.h
+++ config.def.h
@@ -55,6 +55,8 @@ static key keys[] = {
     {  MOD1,             XK_k,          prev_win,          {NULL}},
     {  MOD1|CONTROL,     XK_h,          rotate,            {.i = PREV}},
     {  MOD1|CONTROL,     XK_l,          rotate,            {.i = NEXT}},
+    {  MOD1|SHIFT,       XK_h,          rotate_filled,     {.i = PREV}},
+    {  MOD1|SHIFT,       XK_l,          rotate_filled,     {.i = NEXT}},
     {  MOD1,             XK_Tab,        last_desktop,      {NULL}},
     {  MOD1,             XK_Return,     swap_master,       {NULL}},
     {  MOD1|SHIFT,       XK_j,          move_down,         {NULL}},
