98c550e8a38ac01f5a8dcca0aa1c2c839d1498e0
Ivan Kanakarakis <ivan.kanak@gmail.com>
2012-03-01 19:28:00 +0200

    focus urgent window with a shortcut

--- monsterwm.c
+++ monsterwm.c
@@ -114,6 +114,7 @@ static void deletewindow(Window w);
 static void desktopinfo(void);
 static void destroynotify(XEvent *e);
 static void enternotify(XEvent *e);
+static void focusurgent();
 static unsigned long getcolor(const char* color);
 static void grabbuttons(client *c);
 static void grabkeys(void);
@@ -351,6 +352,12 @@ void enternotify(XEvent *e) {
     client *c = wintoclient(e->xcrossing.window);
     if (c && e->xcrossing.mode   == NotifyNormal
           && e->xcrossing.detail != NotifyInferior) update_current(c);
+}
+
+/* find and focus the client which received
+ * the urgent hint in the current desktop */
+void focusurgent() {
+    for (client *c=head; c; c=c->next) if (c->isurgent) update_current(c);
 }
 
 /* get a pixel with the requested color
