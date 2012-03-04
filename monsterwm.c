623ab87667b946aaa2203fdb42c69468b846bbeb
Ivan Kanakarakis <ivan.kanak@gmail.com>
2012-03-04 03:57:23 +0200

    resize master area with shortcuts

--- monsterwm.c
+++ monsterwm.c
@@ -77,6 +77,7 @@ typedef struct client {
 } client;
 
 /* properties of each desktop
+ * master_size  - the size of the master window
  * mode         - the desktop's tiling layout mode
  * head         - the start of the client list
  * current      - the currently highlighted window
@@ -84,7 +85,7 @@ typedef struct client {
  * showpanel    - the visibility status of the panel
  */
 typedef struct {
-    int mode;
+    int master_size, mode;
     client *head, *current, *prevfocus;
     Bool showpanel;
 } desktop;
@@ -133,6 +134,7 @@ static void prev_win();
 static void propertynotify(XEvent *e);
 static void quit(const Arg *arg);
 static void removeclient(client *c);
+static void resize_master(const Arg *arg);
 static void rotate(const Arg *arg);
 static void rotate_filled(const Arg *arg);
 static void run(void);
@@ -159,7 +161,7 @@ static Bool running = True, showpanel = SHOW_PANEL;
 static int retval = 0;
 static int previous_desktop = 0, current_desktop = 0;
 static int mode = DEFAULT_MODE;
-static int screen, wh, ww; /* window area width/height - screen height minus the panel height */
+static int screen, master_size, wh, ww;
 static int (*xerrorxlib)(Display *, XErrorEvent *);
 static unsigned int numlockmask = 0, win_unfocus, win_focus;
 static Display *dis;
@@ -672,6 +674,16 @@ void removeclient(client *c) {
     if (cd != nd-1) select_desktop(cd);
 }
 
+/* resize the master window - check for boundary size limits
+ * the size of a window can't be less than MINWSZ
+ */
+void resize_master(const Arg *arg) {
+    int msz = master_size + arg->i;
+    if ((mode == BSTACK ? wh : ww) - msz <= MINWSZ || msz <= MINWSZ) return;
+    master_size = msz;
+    tile();
+}
+
 /* jump and focus the next or previous desktop */
 void rotate(const Arg *arg) {
     change_desktop(&(Arg){.i = (DESKTOPS + current_desktop + arg->i) % DESKTOPS});
@@ -693,6 +705,7 @@ void run(void) {
 /* save specified desktop's properties */
 void save_desktop(int i) {
     if (i < 0 || i >= DESKTOPS) return;
+    desktops[i].master_size = master_size;
     desktops[i].mode        = mode;
     desktops[i].head        = head;
     desktops[i].current     = current;
@@ -704,6 +717,7 @@ void save_desktop(int i) {
 void select_desktop(int i) {
     if (i < 0 || i >= DESKTOPS) return;
     save_desktop(current_desktop);
+    master_size     = desktops[i].master_size;
     mode            = desktops[i].mode;
     head            = desktops[i].head;
     current         = desktops[i].current;
@@ -733,6 +747,7 @@ void setup(void) {
 
     ww = XDisplayWidth(dis,  screen);
     wh = XDisplayHeight(dis, screen) - PANEL_HEIGHT;
+    master_size = ((mode == BSTACK) ? wh:ww) * MASTER_SIZE;
     for (unsigned int i=0; i<DESKTOPS; i++) save_desktop(i);
 
     win_focus = getcolor(FOCUS);
@@ -787,13 +802,13 @@ void spawn(const Arg *arg) {
 /* arrange windows in normal or bottom stack tile */
 void stack(int hh, int cy) {
     client *c = NULL, *t = NULL; Bool b = mode == BSTACK;
-    int n = 0, d = 0, z = b ? ww:hh, ma = (b ? hh:ww) * MASTER_SIZE;
+    int n = 0, d = 0, z = b ? ww:hh, ma = master_size;
 
     /* count stack windows and grab first non-floating, non-fullscreen window */
     for (t = head; t; t=t->next) if (!ISFFT(t)) { if (c) ++n; else c = t; }
 
     /* if there is only one window, it should cover the available screen space
-     * if there is only one stack window (n == 1) then we don't care about the adjustments
+     * if there is only one stack window (n == 1) then we don't care about growth
      * if more than one stack windows (n > 1) on screen then adjustments may be needed
      *   - d is the num of pixels than remain when spliting
      *   the available width/height to the number of windows
@@ -834,6 +849,7 @@ void swap_master() {
 void switch_mode(const Arg *arg) {
     if (mode == arg->i) for (client *c=head; c; c=c->next) c->isfloating = False;
     mode = arg->i;
+    master_size = (mode == BSTACK ? wh:ww) * MASTER_SIZE;
     update_current(current);
     desktopinfo();
 }
