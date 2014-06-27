1355f14c72129b6f6d540fec88f6ec5d01c0eded
Robin Schroer <sulamiification@gmail.com>
2014-06-27 17:09:12 +0200

    made new, saner default settings, adapted manpage

--- config.def.h
+++ config.def.h
@@ -3,92 +3,132 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-/** buttons **/
+/* Button definitions, nothing to edit for you */
 #define MOD1            Mod1Mask    /* ALT key */
 #define MOD4            Mod4Mask    /* Super/Windows key */
 #define CONTROL         ControlMask /* Control key */
 #define SHIFT           ShiftMask   /* Shift key */
 
-/** generic settings **/
-#define MASTER_SIZE     0.52
-#define SHOW_PANEL      True      /* show panel by default on exec */
+/* EDIT THIS: general settings */
+#define MASTER_SIZE     0.6       /* master-stack ratio */
+#define SHOW_PANEL      False     /* show panel by default on exec */
 #define TOP_PANEL       True      /* False mean panel is on bottom */
 #define PANEL_HEIGHT    18        /* 0 for no space for panel, thus no panel */
-#define DEFAULT_MODE    TILE      /* TILE MONOCLE BSTACK GRID */
+#define DEFAULT_MODE    TILE      /* TILE MONOCLE BSTACK GRID FIBONACCI */
 #define ATTACH_ASIDE    True      /* False means new window is master */
 #define FOLLOW_MOUSE    False     /* Focus the window the mouse just entered */
 #define FOLLOW_WINDOW   False     /* Follow the window when moved to a different desktop */
-#define CLICK_TO_FOCUS  False     /* Focus an unfocused window when clicked */
+#define CLICK_TO_FOCUS  True      /* Focus an unfocused window when clicked */
 #define BORDER_WIDTH    2         /* window border width */
-#define FOCUS           "#ff950e" /* focused window border color   */
-#define UNFOCUS         "#444444" /* unfocused window border color */
+#define FOCUS           "#cccccc" /* focused window border color   */
+#define UNFOCUS         "#121212" /* unfocused window border color */
 #define DESKTOPS        4         /* number of desktops - edit DESKTOPCHANGE keys to suit */
 #define DEFAULT_DESKTOP 0         /* the desktop to focus on exec */
 #define MINWSZ          50        /* minimum window size in pixels */
 #define USELESSGAP      8         /* the size of the useless gap in pixels */
 #define MONOCLE_BORDERS False     /* display borders in monocle mode */
 
-/* open applications to specified desktop with specified mode.
- * if desktop is negative, then current is assumed */
+/*
+ * EDIT THIS: applicaton specific rules
+ * open applications to specified desktop with specified mode.
+ * if desktop is negative, then current is assumed
+ * sadly, this can not be empty (for now), so enter something non-existent if
+ * you do not wish to use this functionality
+ */
 static const AppRule rules[] = { \
     /*  class     desktop  follow  float */
     { "MPlayer",     3,    True,   False },
     { "Gimp",        0,    False,  True  },
 };
 
-/* helper for spawning shell commands */
+/* helper for spawning shell commands, usually you don't edit this */
 #define SHCMD(cmd) {.com = (const char*[]){"/bin/sh", "-c", cmd, NULL}}
 
-/** commands **/
+/*
+ * EDIT THIS: commands
+ * Adjust those and add them to the shortcuts below to launch anything you want
+ * by pressing a key (combination)
+ */
 static const char *termcmd[] = { "xterm",     NULL };
 static const char *menucmd[] = { "dmenu_run", NULL };
 
 #define DESKTOPCHANGE(K,N) \
-    {  MOD1,             K,              change_desktop, {.i = N}}, \
-    {  MOD1|ShiftMask,   K,              client_to_desktop, {.i = N}},
+    {  MOD4,             K,              change_desktop, {.i = N}}, \
+    {  MOD4|ShiftMask,   K,              client_to_desktop, {.i = N}},
 
-/** Shortcuts **/
+/*
+ * EDIT THIS: shortcuts
+ * By default, all shortcuts use only Mod4 (+ Shift/Control), but you can use
+ * Mod1 as well if you like to, I just prefer not to.
+ */
 static key keys[] = {
     /* modifier          key            function           argument */
-    {  MOD1,             XK_b,          togglepanel,       {NULL}},
-    {  MOD4,             XK_s,          showhide,          {NULL}},
-    {  MOD1,             XK_BackSpace,  focusurgent,       {NULL}},
-    {  MOD1|SHIFT,       XK_c,          killclient,        {NULL}},
+
+    /* select windows */
+    {  MOD4,             XK_j,          next_win,          {NULL}},
+    {  MOD4,             XK_k,          prev_win,          {NULL}},
+    /* select urgent window */
+    {  MOD4,             XK_BackSpace,  focusurgent,       {NULL}},
+
+    /* move windows */
+    {  MOD4|SHIFT,       XK_j,          move_down,         {NULL}},
+    {  MOD4|SHIFT,       XK_k,          move_up,           {NULL}},
+    /* swap the current window to master */
+    {  MOD4,             XK_Return,     swap_master,       {NULL}},
+    /* move the current window to the center of the screen, floating */
     {  MOD4,             XK_c,          centerwindow,      {NULL}},
-    {  MOD1,             XK_j,          next_win,          {NULL}},
-    {  MOD1,             XK_k,          prev_win,          {NULL}},
-    {  MOD1,             XK_h,          resize_master,     {.i = -10}}, /* decrease size in px */
-    {  MOD1,             XK_l,          resize_master,     {.i = +10}}, /* increase size in px */
-    {  MOD1,             XK_o,          resize_stack,      {.i = -10}}, /* shrink   size in px */
-    {  MOD1,             XK_p,          resize_stack,      {.i = +10}}, /* grow     size in px */
-    {  MOD1|CONTROL,     XK_h,          rotate,            {.i = -1}},
-    {  MOD1|CONTROL,     XK_l,          rotate,            {.i = +1}},
-    {  MOD1|SHIFT,       XK_h,          rotate_filled,     {.i = -1}},
-    {  MOD1|SHIFT,       XK_l,          rotate_filled,     {.i = +1}},
-    {  MOD4,             XK_o,          adjust_gaps,       {.i = -1}},
-    {  MOD4,             XK_p,          adjust_gaps,       {.i = +1}},
-    {  MOD1,             XK_Tab,        last_desktop,      {NULL}},
-    {  MOD1,             XK_Return,     swap_master,       {NULL}},
-    {  MOD1|SHIFT,       XK_j,          move_down,         {NULL}},
-    {  MOD1|SHIFT,       XK_k,          move_up,           {NULL}},
-    {  MOD1|SHIFT,       XK_t,          switch_mode,       {.i = TILE}},
-    {  MOD1|SHIFT,       XK_m,          switch_mode,       {.i = MONOCLE}},
-    {  MOD1|SHIFT,       XK_b,          switch_mode,       {.i = BSTACK}},
-    {  MOD1|SHIFT,       XK_g,          switch_mode,       {.i = GRID}},
-    {  MOD1|SHIFT,       XK_i,          switch_mode,       {.i = FIBONACCI}},
-    {  MOD1|CONTROL,     XK_r,          quit,              {.i = 0}}, /* quit with exit value 0 */
-    {  MOD1|CONTROL,     XK_q,          quit,              {.i = 1}}, /* quit with exit value 1 */
-    {  MOD1|SHIFT,       XK_Return,     spawn,             {.com = termcmd}},
-    {  MOD4,             XK_v,          spawn,             {.com = menucmd}},
-       DESKTOPCHANGE(    XK_F1,                             0)
-       DESKTOPCHANGE(    XK_F2,                             1)
-       DESKTOPCHANGE(    XK_F3,                             2)
-       DESKTOPCHANGE(    XK_F4,                             3)
+    /* show/hide all windows on all desktops */
+    {  MOD4|SHIFT,       XK_s,          showhide,          {NULL}},
+
+    /* mode selection */
+    {  MOD4|SHIFT,       XK_t,          switch_mode,       {.i = TILE}},
+    {  MOD4|SHIFT,       XK_m,          switch_mode,       {.i = MONOCLE}},
+    {  MOD4|SHIFT,       XK_b,          switch_mode,       {.i = BSTACK}},
+    {  MOD4|SHIFT,       XK_g,          switch_mode,       {.i = GRID}},
+    {  MOD4|SHIFT,       XK_f,          switch_mode,       {.i = FIBONACCI}},
+
+    /* spawn terminal, dmenu, w/e you want to */
+    {  MOD4|SHIFT,       XK_Return,     spawn,             {.com = termcmd}},
+    {  MOD4,             XK_r,          spawn,             {.com = menucmd}},
+    /* kill current window */
+    {  MOD4|SHIFT,       XK_c,          killclient,        {NULL}},
+
+    /* desktop selection */
+       DESKTOPCHANGE(    XK_1,                             0)
+       DESKTOPCHANGE(    XK_2,                             1)
+       DESKTOPCHANGE(    XK_3,                             2)
+       DESKTOPCHANGE(    XK_4,                             3)
+    /* toggle to last desktop */
+    {  MOD4,             XK_Tab,        last_desktop,      {NULL}},
+    /* jump to the next/previous desktop */
+    {  MOD4|CONTROL,     XK_h,          rotate,            {.i = -1}},
+    {  MOD4|CONTROL,     XK_l,          rotate,            {.i = +1}},
+    /* jump to the next/previous desktop with windows */
+    {  MOD4|CONTROL|SHIFT, XK_h,        rotate_filled,     {.i = -1}},
+    {  MOD4|CONTROL|SHIFT, XK_l,        rotate_filled,     {.i = +1}},
+
+    /* resize master/first stack window */
+    {  MOD4,             XK_h,          resize_master,     {.i = -10}},
+    {  MOD4,             XK_l,          resize_master,     {.i = +10}},
+    {  MOD4,             XK_o,          resize_stack,      {.i = -10}},
+    {  MOD4,             XK_p,          resize_stack,      {.i = +10}},
+
+    /* resize the useless gaps between the windows */
+    {  MOD4|SHIFT,       XK_o,          adjust_gaps,       {.i = -1}},
+    {  MOD4|SHIFT,       XK_p,          adjust_gaps,       {.i = +1}},
+    /* toggle the panel space */
+    {  MOD4|CONTROL,     XK_b,          togglepanel,       {NULL}},
+
+    /* exit with different return values */
+    {  MOD4|CONTROL,     XK_r,          quit,              {.i = 0}},
+    {  MOD4|CONTROL,     XK_q,          quit,              {.i = 1}},
 };
 
+/* EDIT THIS: mouse-based shortcuts */
 static Button buttons[] = {
-    {  MOD1,    Button1,     mousemotion,   {.i = MOVE}},
-    {  MOD1,    Button3,     mousemotion,   {.i = RESIZE}},
-    {  MOD4,    Button3,     spawn,         {.com = menucmd}},
+    /* move/resize using the mouse */
+    {  MOD4,    Button1,     mousemotion,   {.i = MOVE}},
+    {  MOD4,    Button3,     mousemotion,   {.i = RESIZE}},
 };
 #endif
+
