83408a202605c121acad9a3d25f597385a79792f
Robin Schroer <sulamiification@gmail.com>
2014-10-28 21:46:41 +0100

    fix desktopinfo() segfaulting by trying to read nonexistent window titles

--- frankenwm.c
+++ frankenwm.c
@@ -734,8 +734,8 @@ void desktopinfo(void)
         fprintf(stdout, "%d:%d:%d:%d:%d ", d, n, mode, current_desktop == cd,
                 urgent);
         if (d + 1 == DESKTOPS)
-            fprintf(stdout, OUTPUT_TITLE && wtitle.strings ? "%s\n" : "\n",
-                    wtitle.strings);
+            fprintf(stdout, current && OUTPUT_TITLE && wtitle.strings ? "%s\n"
+                    : "\n", wtitle.strings);
     }
     fflush(stdout);
     if (cd != d - 1)
