07823932df5525a41973407bcbebcd0d1be8aaaf
Robin Schroer <sulamiification@gmail.com>
2014-10-29 17:03:59 +0100

    fix desktopinfo once again

--- frankenwm.c
+++ frankenwm.c
@@ -734,8 +734,8 @@ void desktopinfo(void)
         fprintf(stdout, "%d:%d:%d:%d:%d ", d, n, mode, current_desktop == cd,
                 urgent);
         if (d + 1 == DESKTOPS)
-            fprintf(stdout, current && OUTPUT_TITLE && wtitle.strings ? "%s\n"
-                    : "\n", wtitle.strings);
+            fprintf(stdout, "%s\n", current && OUTPUT_TITLE && wtitle.strings ?
+                    wtitle.strings : "");
     }
     fflush(stdout);
     if (cd != d - 1)
