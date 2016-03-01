4971184eb5c4e6b72220fd3f0609c61d433ca71a
t4nkw4rt <t4nkw4rt@gmx.ch>
2016-03-01 20:06:05 +0100

    improved _NET_WM_STATE_HIDDEN handling in setup(); #3

--- frankenwm.c
+++ frankenwm.c
@@ -2673,15 +2673,13 @@ int setup(int default_screen)
  * case 1: window has no desktop property and is unmapped --> ignore
  * case 2: window has no desktop property and is mapped --> add desktop property and append to client list.
  * case 3: window has current desktop property and is unmapped --> map and append to client list.
- * case 4: window has HIDDEN property -> move to miniq and append to client list.
+ * case 4: window has desktop and hidden property -> move to miniq and append to client list.
  * case 5: window has current desktop property and is mapped  --> append to client list.
  * case 6: window has different desktop property and is unmapped -> append to client list.
  * case 7: window has different desktop property and is mapped -> unmap and append to client list.
  * case 8: window has desktop property > DESKTOPS -> move window to last desktop
  * case 9: window has desktop property = -1 -> TODO: sticky window support.
  */
-                if (isHidden)
-                    doMinimize = True;                                                  /* case 4 */
                 if (!(xcb_ewmh_get_wm_desktop_reply(ewmh,
                       xcb_ewmh_get_wm_desktop(ewmh, children[i]), &dsk, NULL))) {
                     if (attr->map_state == XCB_MAP_STATE_UNMAPPED)
@@ -2690,6 +2688,8 @@ int setup(int default_screen)
                         xcb_ewmh_set_wm_desktop(ewmh, children[i], dsk = cd);           /* case 2 */
                 }
                 else {
+                    if (isHidden)
+                        doMinimize = True;                                              /* case 4 */
                     if ((int)dsk > DESKTOPS-1)
                         xcb_ewmh_set_wm_desktop(ewmh, children[i], dsk = DESKTOPS-1);   /* case 8 */
                     if (dsk == cd) {
